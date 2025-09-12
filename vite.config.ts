58a44eb7e2d55196a6a551c6601c0435c2fc553f
Tufan Butuner <Tufan.Butuner@cps.gov.uk>
2025-09-12 11:01:50 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,93 @@
+/// <reference types="vitest" />
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      __MAINTENANCE_MODE__: JSON.stringify(env.VITE_MAINTENANCE_MODE === 'true')
+    },
+    plugins: [
+      react(),
+      basicSsl({ name: 'hk-ui', domains: ['localhost'] }),
+      viteCompression({ algorithm: 'brotliCompress' }),
+      {
+        name: 'inject-external-script',
+        transformIndexHtml(html) {
+          if (env.VITE_GLOBAL_SCRIPT_URL) {
+            console.log(env.VITE_GLOBAL_SCRIPT_URL);
+            return html.replace(
+              '</head>',
+              `<script src="${env.VITE_GLOBAL_SCRIPT_URL}" type="module"></script>\n</head>`
+            );
+          }
+          return html;
+        }
+      }
+    ],
+    resolve: {
+      alias: [{ find: 'node_modules', replacement: '/node_modules' }],
+      dedupe: ['react', 'react-dom']
+    },
+    server: { port: 3001 },
+    build: {
+      outDir: 'build',
+      target: 'esnext',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('msal-browser') || id.includes('msal-react')) {
+                return 'vendor-auth'; // Microsoft Authentication chunk
+              }
+              if (id.includes('axios') || id.includes('swr')) {
+                return 'vendor-fetch'; // Networking libraries
+              }
+              if (
+                id.includes('govuk-frontend') ||
+                id.includes('@ministryofjustice/frontend') ||
+                id.includes('home-office-kit')
+              ) {
+                return 'vendor-govuk'; // UK government UI libraries
+              }
+              if (
+                id.includes('@microsoft/applicationinsights-web') ||
+                id.includes('@microsoft/applicationinsights-react-js')
+              ) {
+                return 'vendor-monitoring'; // Microsoft Application Insights
+              }
+              return 'vendor'; // Default fallback for other dependencies
+            }
+          }
+        }
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern',
+          silenceDeprecations: [
+            'legacy-js-api',
+            'import',
+            'mixed-decls',
+            'slash-div',
+            'global-builtin',
+            'function-units'
+          ]
+        }
+      }
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+      silent: true,
+      exclude: ['e2e', 'node_modules']
+    }
+  };
+});
