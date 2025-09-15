d9a9a5a4012014e62252858152b086d7d01a972b
James Chapman <james.chapman@cps.gov.uk>
2025-09-15 10:04:57 +0100

    State (zustand) example

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
 import { Routes } from './routes.tsx';
 
+import { CaseInfo } from './components';
+
 export const App = () => {
   return (
     <div className="govuk-width-container">
       <cps-global-header></cps-global-header>
+      <CaseInfo />
       <Routes />
     </div>
   );
---
+++ src/stores/index.ts
@@ -0,0 +1,1 @@
+export { useCaseInfoStore } from './useCaseInfo';
