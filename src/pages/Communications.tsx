29913e74c217338b5915ed3a2896f75a68802cac
Tufan Butuner <Tufan.Butuner@cps.gov.uk>
2025-10-07 17:33:02 +0100

    feature/communications: add comms tab and filters

---
+++ src/pages/Communications.tsx
@@ -0,0 +1,147 @@
+import { useContext, useState } from 'react';
+import '../App.scss';
+import {
+  CommsFilters,
+  CommunicationsTable,
+  LoadingSpinner,
+  RenameDrawer,
+  TableActions,
+  TwoCol
+} from '../components';
+
+import { URL } from '../constants/url.ts';
+import { SelectedItemsContext } from '../context/SelectedItemsContext';
+import {
+  useBanner,
+  useCaseInfoStore,
+  useCaseMaterial,
+  useCaseMaterials,
+  useFeatureFlag
+} from '../hooks';
+import { useTableActions } from '../hooks/useTableActions.ts';
+
+export const CommunicationsPage = () => {
+  const hasAccess = useFeatureFlag();
+  const { setBanner, resetBanner } = useBanner();
+  const { loading: caseMaterialsLoading, mutate: refreshCommunications } =
+    useCaseMaterials('communications');
+  const { deselectMaterial } = useCaseMaterial();
+
+  const { caseInfo } = useCaseInfoStore();
+
+  const [showFilter, setShowFilter] = useState(true);
+  const { selectedItems } = useContext(SelectedItemsContext);
+
+  const [isRenameDrawerOpen, setIsRenameDrawerOpen] = useState(false);
+  const [renamedMaterialId, setRenamedMaterialId] = useState<number | null>(
+    null
+  );
+
+  const {
+    handleRenameClick,
+    handleReclassifyClick,
+    handleRedactClick,
+    handleDiscardClick,
+    handleReadStatusClick,
+    handleUnusedClick,
+    determineReadStatusLabel,
+    isReadStatusUpdating
+  } = useTableActions({
+    selectedItems,
+    refreshData: refreshCommunications,
+    setBanner,
+    deselectItem: deselectMaterial,
+    caseInfoData: caseInfo || undefined,
+    resetBanner,
+    setIsRenameDrawerOpen,
+    setRenamedMaterialId
+  });
+
+  const row = selectedItems[0];
+
+  const menuItems = [
+    {
+      label: 'Rename',
+      onClick: () => handleRenameClick(),
+      hide:
+        [1031, 1059].includes(row?.documentTypeId) || selectedItems.length > 1
+    },
+    {
+      label: 'Reclassify',
+      onClick: () => handleReclassifyClick(),
+      hide:
+        !hasAccess([5]) || !row?.isReclassifiable || selectedItems.length > 1
+    },
+    {
+      label: 'Redact',
+      onClick: () => handleRedactClick(row.materialId),
+      hide: !hasAccess([2, 3, 4, 5]) || selectedItems.length > 1
+    },
+    {
+      label: 'Discard',
+      onClick: () => handleDiscardClick(URL.COMMUNICATIONS),
+      hide: !hasAccess([2, 3, 4, 5]) || selectedItems.length > 1
+    },
+    {
+      label: determineReadStatusLabel(selectedItems),
+      onClick: () => handleReadStatusClick(selectedItems),
+      hide: !hasAccess([2, 3, 4, 5])
+    },
+    {
+      label: 'Mark as unused',
+      onClick: () => handleUnusedClick(URL.COMMUNICATIONS)
+    }
+  ];
+
+  return (
+    <div className="govuk-main-wrapper">
+      {isRenameDrawerOpen && (
+        <RenameDrawer
+          material={row}
+          onCancel={() => {
+            setIsRenameDrawerOpen(false);
+          }}
+          onSuccess={async () => {
+            await refreshCommunications();
+
+            setRenamedMaterialId(row.id);
+            setIsRenameDrawerOpen(false);
+            deselectMaterial();
+            setBanner({
+              type: 'success',
+              header: 'Renaming successful',
+              content: 'Material successfully renamed.'
+            });
+          }}
+        />
+      )}
+
+      <TwoCol sidebar={showFilter ? <CommsFilters /> : undefined}>
+        {caseMaterialsLoading ? (
+          <LoadingSpinner />
+        ) : isReadStatusUpdating ? (
+          <LoadingSpinner textContent="Updating read status..." />
+        ) : (
+          <>
+            <TableActions
+              showFilter={showFilter}
+              onSetShowFilter={setShowFilter}
+              menuItems={menuItems}
+            />
+            <CommunicationsTable
+              renamedMaterialId={renamedMaterialId}
+              setRenamedMaterialId={setRenamedMaterialId}
+            />
+
+            {/* <div className="action-on-selection-container">
+              <ButtonMenuComponent
+                menuTitle="Action on selection"
+                menuItems={menuItems}
+              />
+            </div> */}
+          </>
+        )}
+      </TwoCol>
+    </div>
+  );
+};
