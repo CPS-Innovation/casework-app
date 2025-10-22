c014ccd232aa8d5498bbebcce8d7f6178d203b3a
rafalcps <rafal.drozdowski@cps.gov.uk>
2025-10-22 16:00:33 +0100

    removes wrapper component

--- src/caseWorkApp/components/presentation/caseDetails/index.tsx
+++ src/caseWorkApp/components/presentation/caseDetails/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { PageContentWrapper } from '../../PageWrapper';
 import { DocumentArea } from '../../documentArea';
-import classes from './index.module.scss';
 
 export const CaseDetailsWrapper: React.FC<{
   className?: string;
   children?: React.ReactNode;
 }> = ({}) => (
-  <PageContentWrapper>
-    <div className={""}>
-      <DocumentArea></DocumentArea>
-    </div>
-  </PageContentWrapper>
+  <>
+    <DocumentArea></DocumentArea>
+  </>
 );
+
