da66cbae99b5cc5adddfa2667be7f24b14b30405
robmolloy-cps <robert.molloy@cps.gov.uk>
2025-11-05 11:08:59 +0000

    well styled

--- src/packages/DocumentSelectAccordion/DocumentSidebarWrapper.tsx
+++ src/packages/DocumentSelectAccordion/DocumentSidebarWrapper.tsx
@@ -1,6 +1,7 @@
 export const DocumentSidebarWrapper = (p: { children: React.ReactNode }) => {
   return (
-    // <div style={{ background: '#ffffff', border: 'solid 1px #b1b4b6' }}>
-    <div>{p.children}</div>
+    <div style={{ background: '#ffffff', border: 'solid 1px #b1b4b6' }}>
+      {p.children}
+    </div>
   );
 };
