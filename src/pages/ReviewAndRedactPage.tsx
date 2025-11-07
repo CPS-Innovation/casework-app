1dac21557b8c33495ed2ec0afae45fb4401c5963
rafalcps <rafal.drozdowski@cps.gov.uk>
2025-11-07 14:25:43 +0000

    removes unused refs

--- src/pages/ReviewAndRedactPage.tsx
+++ src/pages/ReviewAndRedactPage.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import { DocumentSidebar } from '../packages/DocumentSelectAccordion/DocumentSidebar';
-// import { CaseDetailsWrapper as CaseDetailsPage} from './caseWorkApp/components/presentation/caseDetails';
 import { CaseDetailsWrapper  as CaseDetailsPage} from '../caseWorkApp/components/presentation/caseDetails';
 
 export const ReviewAndRedactPage = () => {
