5faefb3feb68c0079248128ebf924fe9d8cd2526
robmolloy-cps <robert.molloy@cps.gov.uk>
2025-11-13 14:13:50 +0000

    feat/pdf-viewer

--- src/caseWorkApp/pages/ReviewAndRedactPage.tsx
+++ src/caseWorkApp/pages/ReviewAndRedactPage.tsx
@@ -2,11 +2,16 @@ import { useState } from 'react';
 import { TwoCol } from '../../components';
 import { DocumentSidebar } from '../../packages/DocumentSelectAccordion/DocumentSidebar';
 import { PdfViewer } from '../../packages/pdfViewer/PdfViewer';
+import { createId } from '../../packages/pdfViewer/utils/generalUtils';
 import { DocumentControlArea } from '../components/documentControlArea';
 import { DocumentViewportArea } from '../components/documenViewportArea';
 
 export const ReviewAndRedactPage = () => {
   const [openDocumentIds, setOpenDocumentIds] = useState<string[]>([]);
+
+  const [redactionDetails, setRedactionDetails] = useState<
+    { redactionId: string; randomId: string }[]
+  >([]);
 
   const items = [
     {
@@ -42,7 +47,46 @@ export const ReviewAndRedactPage = () => {
 
           <DocumentViewportArea></DocumentViewportArea>
 
-          <PdfViewer fileUrl="http://localhost:3000/test-pdfs/different-orientations-and-sizes.pdf" />
+          <div>
+            {JSON.stringify({ redactionDetails })}
+            <div style={{ maxWidth: '500px' }}>
+              <PdfViewer
+                // fileUrl="http://localhost:5173/may-plus-images.pdf"
+                // fileUrl="http://localhost:5173/final.pdf"
+                fileUrl="http://localhost:5173/final-with-https.pdf"
+                onRedactionsChange={(change) => {
+                  console.log(`OfficialPdfViewer.tsx:${/*LL*/ 16}`, { change });
+                }}
+                onAddRedactions={(add) => {
+                  const newRedactions = add.map((x) => ({
+                    redactionId: x.id,
+                    randomId: `This redaction does ${createId()}`
+                  }));
+                  setRedactionDetails((prev) => [...prev, ...newRedactions]);
+                }}
+                onRemoveRedactions={(remove) => {
+                  setRedactionDetails((prev) =>
+                    prev.filter((x) => !remove.includes(x.redactionId))
+                  );
+                  console.log(`OfficialPdfViewer.tsx:${/*LL*/ 18}`, { remove });
+                }}
+                onSaveRedactions={(redactions) => {
+                  const redactionsWithDetails = redactions
+                    .map((x) => {
+                      const thisDetails = redactionDetails.find(
+                        (y) => y.redactionId === x.id
+                      );
+                      if (!thisDetails) return undefined;
+                      return { ...x, ...thisDetails };
+                    })
+                    .filter((x) => !!x);
+                  console.log(`OfficialPdfViewer.tsx:${/*LL*/ 44}`, {
+                    redactionsWithDetails
+                  });
+                }}
+              />
+            </div>
+          </div>
         </>
       </TwoCol>
     </div>
