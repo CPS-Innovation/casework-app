aa4500b185c10563d62e79eaba17bc428c5e1df9
rafalcps <rafal.drozdowski@cps.gov.uk>
2025-11-14 15:09:25 +0000

    WIP

--- src/caseWorkApp/pages/ReviewAndRedactPage.tsx
+++ src/caseWorkApp/pages/ReviewAndRedactPage.tsx
@@ -3,28 +3,98 @@ import { TwoCol } from '../../components';
 import { DocumentSidebar } from '../../packages/DocumentSelectAccordion/DocumentSidebar';
 import { DocumentControlArea } from '../components/documentControlArea';
 import { DocumentViewportArea } from '../components/documenViewportArea';
+import { GetDataFromAxios } from '../utils/data';
+
+type TU = {
+  cmsOriginalFileName: string;
+  documentId: string;
+  hasNotes: boolean;
+  isUnused: boolean;
+  presentationTitle: string;
+  status: string;
+};
 
 export const ReviewAndRedactPage = () => {
   const [openDocumentIds, setOpenDocumentIds] = useState<string[]>([]);
   const [isSidebarVisible, setIsSidebarVisible] = useState<boolean>(true);
   const [documentIDs, setDocumentIDs] = useState<any[]>([]);
-  const [removedDocumentId, setRemovedDocumentId] = useState<{
-    id: string;
-    label: string;
-    title: string;
-  } | undefined>(undefined);
+  const [removedDocumentId, setRemovedDocumentId] = useState<
+    { id: string; label: string; title: string } | undefined
+  >(undefined);
+  const [documentsDataList, setDocumentsDataList] = useState<TU[]>([]);
+
+  const { useAxiosInstance, getDocuments } = GetDataFromAxios();
+
+  const a = useAxiosInstance();
+
+  useEffect(() => {
+    const getDocument = getDocuments({
+      axiosInstance: a,
+      urn: '54KR7689125',
+      caseId: 2160797
+    }).then((e) => {
+      setDocumentsDataList(e);
+      console.log('dataaaa', e);
+    });
+  }, []);
 
   const handleCloseTab = (v: string) => {
     setRemovedDocumentId(v as any);
   };
 
-  useEffect(() => {
-    const res = openDocumentIds?.map((item) => {
-      return { id: item, label: item, title: item };
-    });
+  // const getDetails = () => {
+  //   openDocumentIds.map((el, index) => {
+  //     return documentsDataList?.filter((item) => {
+  //       if (item.documentId === el) {
+  //         const el = {
+  //           id: item.documentId,
+  //           label: item.presentationTitle,
+  //           title: item.presentationTitle
+  //         };
+  //         setDocumentIDs(el as any);
+  //       }
+  //     });
+  //   });
+  // };
 
-    setDocumentIDs(res);
+  useEffect(() => {
+    console.log('oid: ', openDocumentIds);
+
+    // const res = openDocumentIds?.find((i) => {
+    //    return documentsDataList.find((item) => {
+    //     if ( item.documentId === i ) {
+    //       return {
+    //         id: 'item.documentId',
+    //         label: 'item.presentationTitle',
+    //         title: 'item.presentationTitle'
+    //       }
+    //     }
+    //   });
+    // });
+    // // setDocumentIDs(res as any)
+    // console.log(res);
+
+    const matchingDocuments = documentsDataList.filter((item) => {
+      if(openDocumentIds.includes(item.documentId)) {
+
+        return {
+          id: item.documentId,
+          label: item.presentationTitle,
+          title: item.presentationTitle
+        };
+      }
+    });    
+    setDocumentIDs(matchingDocuments)
+    console.log('md: ', matchingDocuments);
   }, [openDocumentIds]);
+
+  // useEffect(() => {
+  //   const res = openDocumentIds?.map((item) => {
+  //     return { id: item, label: item, title: item };
+  //   });
+
+  //   setDocumentIDs(res);
+  // }, [openDocumentIds]);
 
   useEffect(() => {
     const newArray = documentIDs?.filter((e) => {
---
+++ src/caseWorkApp/utils/data.tsx
@@ -0,0 +1,39 @@
+import { useMsal } from '@azure/msal-react';
+import axios, { AxiosInstance } from 'axios';
+import { getAccessTokenFromMsalInstance } from '../../packages/DocumentSelectAccordion/getters/getAccessTokenFromMsalInstance';
+
+export const useAxiosInstance = () => {
+  const { instance: msalInstance } = useMsal();
+
+  const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_POLARIS_GATEWAY_URL,
+    withCredentials: true
+  });
+
+  axiosInstance.interceptors.request.use(async (config) => {
+    const accessToken = await getAccessTokenFromMsalInstance(msalInstance);
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    config.headers['Correlation-Id'] = crypto.randomUUID();
+
+    return config;
+  });
+
+  return axiosInstance;
+};
+
+export const getDocuments = async (p: {
+  axiosInstance: AxiosInstance;
+  urn: string | undefined;
+  caseId: number | undefined;
+}) => {
+  const response = await p.axiosInstance.get(
+    `/api/urns/${p.urn}/cases/${p.caseId}/documents`
+  );
+
+  return response.data;
+};
+
+export const GetDataFromAxios = () => {
+  return { useAxiosInstance, getDocuments };
+};
+
