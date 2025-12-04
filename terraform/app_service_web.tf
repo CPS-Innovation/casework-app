f65b8f10406a3790a24eae15b357f5690b965e38
dbarber-cps <Daniel.Barber@cps.gov.uk>
2025-12-04 11:19:06 +0000

    Feature/fct2 10777 terraform (#74)
    * initial tf build
    
    * TF release pipeline
    
    * tf release template
    
    * testing template path
    
    * rearranging variable groups at bottom of variables
    
    * updating MaterialsBuild pipeline source name
    
    * updated enviornment in ADO
    
    * restructured folders
    
    * testing variable groups
    
    * variable set to lowercase
    
    * build-pipeline-name variable update
    
    * adding debug steps
    
    * dubugging variables
    
    * V2 version of release pipeline
    
    * corrected template path
    
    * variable updates
    
    * correcting azureSubscription variable
    
    * correcting terraformVersion parameter in appy TF template
    
    * adding env vars to apply template
    
    * updating env variable for trvars location
    
    * terraform locals/variables update
    
    * added location_abbr variable
    
    * removing old release template versions
    
    * adding apply operation back to appy pipeline
    
    * azure app service components
    
    * correcting worker count attribute in asp
    
    * tags update + web_asp_materials variable update
    
    * staging slot for web ui
    
    * typo correction
    
    * adding conditional statement for deployment slots in dev
    
    * naming updates and prep for web app reg
    
    * env tfvars updates
    
    * updates to azuread_service_principal for V3 of provider
    
    * removing random_uuid
    
    * app reg code + modules
    
    * module provider updates
    
    * changing to client ID using SP module since v3 of provider
    
    * changes to be able to use V3 of azuread provider
    
    * tag updates
    
    * tag variable update
    
    * adding azurerm_client_config
    
    * tag update
    
    * tag updates
    
    * tag updates
    
    * added output ID for module service_principal
    
    * name change for component app service
    
    * new QA pipelines
    
    * renaming components web app
    
    * QA tfvars
    
    * increasing SKU to P0v3 for qa
    
    * prod tfvars file
    
    * setting dev to P0v3 to be able to use deployment slots
    
    * removing conditional build for dev staging due to limitations in terraform conditional lifecycle blocks
    
    * tf vars cleanup
    
    * Update .gitignore
    
    * adding checkov blocks to ignore certain events in dev
    
    * removing triggers for pr triggering builds

---
+++ terraform/app_service_web.tf
@@ -0,0 +1,215 @@
+resource "azurerm_linux_web_app" "as_web_materials" {
+  #checkov:skip=CKV_AZURE_66:Ensure that App service enables failed request tracing
+  #checkov:skip=CKV_AZURE_88:Ensure that app services use Azure Files
+  #checkov:skip=CKV_AZURE_213:Ensure that App Service configures health check
+  #checkov:skip=CKV_AZURE_13:Ensure App Service Authentication is set on Azure App Service
+  #checkov:skip=CKV_AZURE_17:Ensure the web app has 'Client Certificates (Incoming client certificates)' set
+  name                          = "as-${local.web_materials_name}"
+  location                      = azurerm_resource_group.rg_materials.location
+  service_plan_id               = azurerm_service_plan.web_linux.id
+  resource_group_name           = azurerm_resource_group.rg_materials.name
+  #virtual_network_subnet_id     = data.azurerm_subnet.materials_subnets[var.ui_subnet_name].id #TBC vnet integration is not required
+  public_network_access_enabled = false
+  https_only                    = true
+  client_certificate_enabled    = false
+
+  site_config {
+    ftps_state              = "FtpsOnly"
+    http2_enabled           = true
+    app_command_line        = "" #TBC
+    always_on               = true
+    vnet_route_all_enabled  = true
+    scm_minimum_tls_version = "1.2"
+
+    application_stack {
+      node_version = "22-lts" #TBC
+    }
+  }
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"  = azurerm_application_insights.ai_materials.instrumentation_key
+    "HostType"                        = "Production"
+  }
+
+  sticky_settings {
+    app_setting_names = ["HostType"]
+  }
+
+/*
+  auth_settings_v2 {
+    auth_enabled           = true
+    require_authentication = true
+    default_provider       = "AzureActiveDirectory"
+    unauthenticated_action = "AllowAnonymous"
+    excluded_paths         = ["/status"]
+
+    # our default_provider:
+    active_directory_v2 {
+      tenant_auth_endpoint = "https://sts.windows.net/${data.azurerm_client_config.current.tenant_id}/v2.0"
+      client_secret_setting_name = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
+      client_id                  = module.azurerm_app_reg_as_web_materials.client_id
+    }
+
+    # use a store for tokens (az blob storage backed)
+    login {
+      token_store_enabled = true
+    }
+  }
+*/
+
+  logs {
+    detailed_error_messages = true
+    failed_request_tracing  = true
+
+    http_logs {
+      file_system {
+        retention_in_days = 7
+        retention_in_mb   = 35
+      }
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      app_settings["APPINSIGHTS_INSTRUMENTATIONKEY"],
+      app_settings["HostType"],
+      tags
+    ]
+  }
+}
+
+## Below app registation will be recreated ##
+module "azurerm_app_reg_as_web_materials" { # Note, app roles are currently being managed outside of terraform and it's functionality has been commented out from the module.
+  source                  = "./modules/terraform-azurerm-azuread-app-registration"
+  display_name            = "as-${local.web_materials_name}-appreg"
+  identifier_uris         = ["https://CPSGOVUK.onmicrosoft.com/as-${local.web_materials_name}"]
+  owners                  = concat([data.azuread_service_principal.terraform_service_principal.object_id], var.app_reg_owners)
+  prevent_duplicate_names = true
+  group_membership_claims = ["ApplicationGroup"]
+  optional_claims = {
+    access_token = {
+      name = "groups"
+    }
+    id_token = {
+      name = "groups"
+    }
+    saml2_token = {
+      name = "groups"
+    }
+  }
+  #use this code for adding api permissions
+  required_resource_access = [{
+    # Microsoft Graph
+    resource_app_id = "00000003-0000-0000-c000-000000000000"  # AppId for Microsoft Graph
+    resource_access = [{
+      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"  # user.read for Microsoft Graph)
+      type = "Scope"
+    }]
+    },
+    {
+      resource_app_id = data.azuread_application.fa_polaris_gateway.client_id
+      resource_access = [{
+        id   = data.azuread_application.fa_polaris_gateway.oauth2_permission_scope_ids["user_impersonation"]
+        type = "Scope"
+      }]
+    }
+    ]
+
+  single_page_application = {
+    redirect_uris = ["https://as-${local.web_materials_name}.azurewebsites.net/${var.materials_ui_sub_folder}", "http://localhost:3000/${var.materials_ui_sub_folder}", "https://${local.web_materials_name}.cps.gov.uk/${var.materials_ui_sub_folder}"]
+  }
+  api = {
+    mapped_claims_enabled          = true
+    requested_access_token_version = 1
+  }
+  web = {
+    homepage_url  = "https://as-${local.web_materials_name}.azurewebsites.net"
+    redirect_uris = ["https://getpostman.com/oauth2/callback"]
+    implicit_grant = {
+      access_token_issuance_enabled = true
+      id_token_issuance_enabled     = true
+    }
+  }
+  tags = ["created by terraform"]
+}
+
+resource "azuread_application_password" "asap_web_materials_app_service" {
+  application_id    = module.azurerm_app_reg_as_web_materials.id
+  end_date_relative = "17520h"
+}
+
+# # Create life cycle for e2e-tests' version of the client secret
+# resource "time_rotating" "schedule" {
+#   rotation_days = 90
+# }
+
+# Will most likely be needed in future
+# resource "azuread_application_password" "e2e_test_secret" {
+#   application_object_id = module.azurerm_app_reg_as_web_materials.object_id
+#   display_name          = "e2e-tests client secret"
+#   rotate_when_changed = {
+#     rotation = time_rotating.schedule.id
+#   }
+# }
+
+module "azurerm_service_principal_sp_materials_web" { # Note, app roles are currently being managed outside of terraform and it's functionality has been commented out from the module.
+  source                       = "./modules/terraform-azurerm-azuread_service_principal"
+  client_id                    = module.azurerm_app_reg_as_web_materials.client_id
+  app_role_assignment_required = false
+  owners                       = concat([data.azurerm_client_config.current.object_id], var.app_reg_owners)
+  depends_on                   = [module.azurerm_app_reg_as_web_materials]
+}
+
+resource "azuread_service_principal_password" "sp_materials_web_pw" {
+  service_principal_id = module.azurerm_service_principal_sp_materials_web.id
+}
+
+resource "azuread_application_pre_authorized" "fapre_materials_web" { # Adding the App Reg we created above as an authorized app to the App Reg for the backend polaris function app
+  application_id       = data.azuread_application.fa_polaris_gateway.id
+  authorized_client_id = module.azurerm_app_reg_as_web_materials.client_id
+  permission_ids       = [data.azuread_application.fa_polaris_gateway.oauth2_permission_scope_ids["user_impersonation"]]
+}
+
+resource "azuread_service_principal_delegated_permission_grant" "materials_web_grant_access_to_msgraph" {
+  service_principal_object_id          = module.azurerm_service_principal_sp_materials_web.object_id
+  resource_service_principal_object_id = azuread_service_principal.msgraph.object_id
+  claim_values                         = ["User.Read"]
+}
+
+resource "azurerm_private_endpoint" "pep_as_web_materials" {
+  name                = "${azurerm_linux_web_app.as_web_materials.name}-pe"
+  location            = azurerm_resource_group.rg_materials.location
+  resource_group_name = azurerm_resource_group.rg_materials.name
+  subnet_id           = data.azurerm_subnet.materials_subnets[var.app_subnet_name].id
+
+  private_service_connection {
+    name                           = azurerm_linux_web_app.as_web_materials.name
+    private_connection_resource_id = azurerm_linux_web_app.as_web_materials.id
+    subresource_names              = ["sites"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "app-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.hub_dns_zones["sites"].id]
+  }
+
+/*
+  dynamic "ip_configuration" {
+    for_each = var.as_web_pe_ip == null ? [] : [1]
+    content {
+      name               = "ip-${azurerm_linux_web_app.as_web_materials.name}"
+      private_ip_address = var.as_web_pe_ip
+      subresource_name   = "sites"
+      member_name        = "sites"
+    }
+  }
+*/
+  tags = local.common_tags
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,110 @@
+## Global ##
+variable "project_name" {
+  description = "The project name of this resource"
+  type    = string
+}
+
+variable "environment" {
+  description = "The environment of this resource"
+  type = string
+}
+
+variable "location" {
+  description = "The location of this resource"
+  type        = string
+}
+
+variable "resource_name_prefix" {
+  type    = string
+  default = "polaris"
+}
+
+variable "location_abbr" {
+  description = "The abbreviated located of this resource"
+  type        = string
+}
+
+variable "environment_tag" {
+  type        = string
+  description = "Environment tag value"
+}
+
+variable "terraform_service_principal_display_name" {
+  type = string
+  description = "Name of the service principal responsible for running Terraform"
+}
+
+## Networking ##
+variable "private_dns_zones" {
+  type        = map(string)
+  description = "A map of private dns zones"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "The name of the virtual network"
+}
+
+variable "vnet_rg" {
+  type        = string
+  description = "The name of the resource group containing the VNet."
+}
+
+variable "app_subnet_name" {
+  type = string
+}
+
+variable "ui_subnet_name" {
+  type = string
+}
+
+variable "dns_server" {
+  type        = string
+  description = "The name of the DNS server"
+}
+
+variable "as_web_pe_ip" {
+  type        = string
+  description = "A static private IP address to use for the Materials UI private endpoint."
+  default     = null
+}
+
+variable "as_comp_pe_ip" {
+  type        = string
+  description = "A static private IP address to use for the Materials UI private endpoint."
+  default     = null
+}
+
+## UI ##
+variable "web_asp_materials" {
+  type = object({
+    sku     = string
+    worker_count           = string
+    zone_balancing_enabled = string
+    autoscale_default      = string
+    autoscale_minimum      = string
+    autoscale_maximum      = string 
+  })
+}
+
+variable "app_reg_owners" {
+  description = "List of object IDs for users with owner permissions to App Registrations we are creating during this build "
+  type        = list(string)
+  default     = []
+}
+
+# Lookups
+variable "fa_polaris_gateway_name" {
+  type        = string
+  description = "The name of the polaris function app backend."
+  default     = null
+}
+
+
+variable "materials_ui_sub_folder" {
+  type = string
+  // this value must match the PUBLIC_URL=... value
+  //  as seen in the ui project top-level package.json
+  //  scripts section.
+  default = "materials-ui"
+}
