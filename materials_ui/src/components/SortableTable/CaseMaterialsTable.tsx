b4db12142129ea41d5d0270ef68b85a6a355c8ee
dbarber-cps <Daniel.Barber@cps.gov.uk>
2025-12-09 14:13:19 +0000

    creating root folder for UI (#84)

---
+++ materials_ui/src/components/SortableTable/CaseMaterialsTable.tsx
@@ -0,0 +1,177 @@
+import { useEffect, useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { useCaseMaterials, useFilters, usePager } from '../../hooks';
+
+import { CaseMaterialsType } from '../../schemas';
+import {
+  defaultFilterFn,
+  defaultSearchFn,
+  defaultSortFn
+} from '../../utils/filtering';
+import SortableTable, { Column } from './SortableTable';
+
+import { DEFAULT_RESULTS_PER_PAGE } from '../../constants/query';
+
+import { READ_STATUS } from '../../constants';
+import { formatDate } from '../../utils/date';
+
+import {
+  DocumentPreview,
+  LoadingSpinner,
+  Pagination,
+  StatusTag
+} from '..';
+import { useMaterialTags } from '../../stores';
+
+export const CaseMaterialsTable = () => {
+  const [queryParams] = useSearchParams();
+  const {
+    filteredData,
+    loading: caseMaterialsLoading,
+    error
+  } = useCaseMaterials({ dataType: 'materials' });
+  const { filters } = useFilters('materials');
+  const { materialTags } = useMaterialTags();
+
+  const filteredSortedData = useMemo(() => {
+    const sortFn = defaultSortFn<CaseMaterialsType>(filters?.sort);
+    const sortByStatusFn = defaultSortFn<CaseMaterialsType>({
+      column: 'statusLabel',
+      direction: 'descending'
+    });
+    const filterFn = defaultFilterFn<CaseMaterialsType>(filters?.filters);
+    const searchFn = defaultSearchFn<CaseMaterialsType>(
+      'subject',
+      filters?.search
+    );
+
+    return filteredData
+      ?.map((material) => {
+        const materialTag = materialTags.find(
+          (materialTag) => materialTag.materialId === material.materialId
+        );
+
+        return materialTag
+          ? { ...material, statusLabel: materialTag.tagName }
+          : material;
+      })
+      ?.filter(searchFn)
+      ?.filter(filterFn)
+      ?.sort((a, b) => {
+        if (
+          a.readStatus === READ_STATUS.UNREAD &&
+          b.readStatus !== READ_STATUS.UNREAD
+        ) {
+          return -1;
+        }
+        if (
+          a.readStatus !== READ_STATUS.READ &&
+          b.readStatus === READ_STATUS.READ
+        ) {
+          return 1;
+        }
+        return 0;
+      })
+      ?.sort(sortFn)
+      ?.sort(sortByStatusFn);
+  }, [filters, filteredData, materialTags]);
+
+  const currentPageParam = queryParams?.get('page');
+
+  const {
+    currentPage,
+    totalPages,
+    startIndex,
+    endIndex,
+    setNextPage,
+    setPage,
+    setPreviousPage
+  } = usePager({
+    totalItems: filteredSortedData?.length,
+    initialPageSize: DEFAULT_RESULTS_PER_PAGE,
+    initialPage: currentPageParam ? +currentPageParam - 1 : 0
+  });
+
+  const columns: Column<CaseMaterialsType>[] = [
+    {
+      key: 'subject',
+      heading: 'Material',
+      render: (row) => (
+        <>
+          {row.readStatus == READ_STATUS.UNREAD && <StatusTag status="New" />}
+          <span className="subject-field">{row.subject}</span>
+          {row.statusLabel && <StatusTag status={row.statusLabel} />}
+        </>
+      ),
+      isSortable: true
+    },
+    { key: 'type', heading: 'Type', isSortable: true },
+    { key: 'category', heading: 'Category', isSortable: true },
+    {
+      key: 'date',
+      heading: 'Date',
+      render: (row) => (
+        <span aria-label={row.date === null ? 'No date available' : undefined}>
+          {formatDate(row.date)}
+        </span>
+      ),
+      isSortable: true
+    },
+    {
+      key: 'status',
+      heading: 'Status',
+      render: (row) => <StatusTag status={row.status} />,
+      isSortable: true
+    }
+  ];
+
+  const expandableRow = (row: CaseMaterialsType) => (
+    <DocumentPreview row={row} />
+  );
+
+  const recordsOnCurrentPage = endIndex + 1 - startIndex;
+
+  useEffect(() => {
+    setPage(0);
+  }, [filters, setPage]);
+
+  if (caseMaterialsLoading) {
+    return <LoadingSpinner textContent="Loading materials..." />;
+  }
+
+  return (
+    <>
+      <p className="govuk-body showing-materials-count">
+        Showing{' '}
+        <strong>
+          {filteredSortedData?.length === 0 ? 0 : recordsOnCurrentPage}
+        </strong>{' '}
+        materials out of <strong>{filteredSortedData?.length}</strong>
+      </p>
+      <SortableTable
+        data={(filteredSortedData || []).slice(startIndex, endIndex + 1)}
+        dataName="materials"
+        caption="Case materials list view"
+        filters={filters}
+        columns={columns}
+        expandableRow={expandableRow}
+        error={error}
+      />
+
+      <div
+        className={
+          totalPages > 1 ? 'table-actions-footer' : 'table-actions-footer-end'
+        }
+      >
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          setNextPage={setNextPage}
+          setPreviousPage={setPreviousPage}
+          setPage={setPage}
+        />
+      </div>
+    </>
+  );
+};
---
+++ materials_ui/src/hooks/useCaseWitnesses.ts
@@ -0,0 +1,64 @@
+import useSWR from 'swr';
+
+import type { SelectOption } from '../components/SelectList/SelectList';
+import { QUERY_KEYS } from '../constants/query';
+import { useRequest } from '.';
+import type {
+  WitnessListItemType,
+  WitnessListResponseType
+} from '../schemas/witness';
+import { useCaseInfoStore } from '../stores';
+
+export const useCaseWitnesses = () => {
+  const request = useRequest();
+  const { caseInfo } = useCaseInfoStore();
+
+  const getCaseWitnesses = async () =>
+    await request
+      .get<WitnessListResponseType>(
+        `urns/${caseInfo?.urn}/cases/${caseInfo?.id}/case-witnesses`,
+        { params: { caseId: caseInfo?.id } }
+      )
+      .then((response) => response.data);
+
+  const { data: caseWitnesses, isLoading } = useSWR(
+    caseInfo ? QUERY_KEYS.CASE_WITNESSES : null,
+    getCaseWitnesses
+  );
+
+  const formatWitnessName = (witness?: WitnessListItemType | null): string => {
+    if (!witness) {
+      return '';
+    }
+
+    return `${witness?.firstName} ${witness?.surname}`;
+  };
+  const getWitnessById = (
+    witnessId?: number | string
+  ): WitnessListItemType | null => {
+    if (!witnessId) {
+      return null;
+    }
+
+    const witness = caseWitnesses?.witnesses?.find(
+      (witness) => witness.witnessId.toString() === witnessId?.toString()
+    );
+
+    return witness || null;
+  };
+
+  const selectOptions: SelectOption[] =
+    caseWitnesses?.witnesses?.map((witness) => ({
+      id: witness?.witnessId,
+      label: formatWitnessName(witness) || '',
+      value: witness?.witnessId
+    })) || [];
+
+  return {
+    getWitnessById,
+    loading: isLoading,
+    data: caseWitnesses?.witnesses || [],
+    selectOptions,
+    formatWitnessName
+  };
+};
