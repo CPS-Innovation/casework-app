e405a726c59bbf74f5cebefb01640122f2328243
James Chapman <james.chapman@cps.gov.uk>
2025-12-11 12:20:09 +0000

    FCT2-11414: case search refinements

--- materials_ui/src/hooks/search/useCaseSearch.ts
+++ materials_ui/src/hooks/search/useCaseSearch.ts
@@ -1,20 +1,36 @@
 import useSWR from 'swr';
 
-import { useRequest } from '..';
+import { useBanner, useRequest } from '..';
 import { QUERY_KEYS } from '../../constants/query';
 import { CaseDetailsType } from '../../schemas/caseDetails';
 
 export const useCaseSearch = (urn: string | undefined) => {
   const request = useRequest();
+  const { resetBanner, setBanner } = useBanner();
 
-  const getCase = async () =>
-    await request
+  const getCase = async () => {
+    resetBanner();
+
+    return await request
       .get<CaseDetailsType>(`/urns/${urn}/cases`)
       .then((response) => response.data);
+  };
 
   const { data, isLoading, isValidating, mutate } = useSWR(
     urn ? [QUERY_KEYS.CASE_SEARCH, urn] : null,
-    getCase
+    getCase,
+    {
+      onError: (error) => {
+        if (error.status === 500) {
+          setBanner({
+            type: 'error',
+            header: 'Something went wrong',
+            content:
+              'There was a problem with the server when searching for a case.'
+          });
+        }
+      }
+    }
   );
 
   return {
