bbdd9922a7f665288c631757c59341369bea1451
dbarber-cps <Daniel.Barber@cps.gov.uk>
2026-01-06 13:30:28 +0000

    Fct2 11413 app deploy pipeline (#108)
    * Build pipeline
    
    * removing package-lock.json from gitignore
    
    * new UI release pipeline
    
    * removing prettier steps from build. Moving to PR pipeline instead
    
    * added env vars into build steps
    
    * package.lock file
    
    * adding task to check for running releases
    
    * correcting path of running releases
    
    * corrected path for task_deploy-spa.yml
    
    * added task for job_swap-slots
    
    * correcting typo in stage Swap_Slots
    
    * correcting typo in CI_Deploy_Materials_UI
    
    * added variable for "dev-azure-subscription"
    
    * removing typo
    
    * Removing unused dependencies
    
    * build artifact name updates
    
    * added artifact variables to release
    
    * moving some variables into variable groups for build
    
    * typo on release pipeline
    
    * enabling private endpoint on staging deployment slot in dev
    
    * setting AzureRmWebAppDeployment to v4
    
    * testing SPA release download steps
    
    * adding npx serve to startup
    
    * adding npx server to staging slot
    
    * adding in steps to build artifacts for scripts
    
    * new script
    
    * hashed out testing stage to progress once site is live
    
    * resolving typo in scripts dir
    
    * updating triggers for pipelines
    
    * Delete materials_devops_pipelines/scripts/InvokeRequestWithRetry.ps1

--- terraform/app_service_web.tf
+++ terraform/app_service_web.tf
@@ -16,7 +16,7 @@ resource "azurerm_linux_web_app" "as_web_materials" {
   site_config {
     ftps_state              = "FtpsOnly"
     http2_enabled           = true
-    app_command_line        = "" #TBC
+    app_command_line       = "npx serve -s"
     always_on               = true
     vnet_route_all_enabled  = true
     scm_minimum_tls_version = "1.2"
