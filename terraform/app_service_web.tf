ae8dc741477408b9a758e4678c9ce9ecfc65a7dc
Daniel Barber <daniel.barber@cps.gov.uk>
2026-02-02 15:51:18 +0000

    terraform changes for redirect URIs on app reg

--- terraform/app_service_web.tf
+++ terraform/app_service_web.tf
@@ -120,11 +120,9 @@ module "azurerm_app_reg_as_web_materials" { # Note, app roles are currently bein
       }]
     }
     ]
-
   single_page_application = {
-    redirect_uris = ["https://as-${local.web_materials_name}.azurewebsites.net/", "https://as-${local.web_materials_name}.azurewebsites.net/${var.materials_ui_sub_folder}", "https://${local.web_materials_name}.cps.gov.uk/${var.materials_ui_sub_folder}"]
+    redirect_uris = var.env != "prod" ? ["https://as-${local.web_materials_name}.azurewebsites.net/${var.materials_ui_sub_folder}", "http://localhost:3000/${var.materials_ui_sub_folder}", "https://${local.polaris_name_map[var.env]}-notprod.cps.gov.uk/${var.materials_ui_sub_folder}"] : ["https://as-${local.web_materials_name}.azurewebsites.net/${var.materials_ui_sub_folder}", "http://localhost:3000/${var.materials_ui_sub_folder}", "https://${local.polaris_name_map[var.env]}.cps.gov.uk/${var.materials_ui_sub_folder}"]
   }
-
   api = {
     mapped_claims_enabled          = true
     requested_access_token_version = 1
