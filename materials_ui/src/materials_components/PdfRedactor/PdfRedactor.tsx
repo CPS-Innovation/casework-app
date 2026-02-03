68092e009b7a753c8832c886b171742e79a3e697
robmolloy-cps <robert.molloy@cps.gov.uk>
2026-02-03 11:11:09 +0000

    refactor/base-url-2

--- materials_ui/src/materials_components/PdfRedactor/PdfRedactor.tsx
+++ materials_ui/src/materials_components/PdfRedactor/PdfRedactor.tsx
@@ -42,6 +42,7 @@ const RedactionsFooter = (p: {
   redactions: TRedaction[];
   onRemoveAllRedactionsClick: () => void;
   onSaveRedactionsClick: (x: TRedaction[]) => void;
+  onShowRedactionLogModal?: (redactions: TRedaction[]) => void;
 }) => {
   return (
     <div
@@ -70,7 +71,12 @@ const RedactionsFooter = (p: {
         </span>
         <button
           className="govuk-button"
-          onClick={() => p.onSaveRedactionsClick(p.redactions)}
+          onClick={() => {
+            p.onSaveRedactionsClick(p.redactions);
+            if (p.onShowRedactionLogModal) {
+              p.onShowRedactionLogModal(p.redactions);
+            }
+          }}
         >
           Save all redactions
         </button>
@@ -194,6 +200,7 @@ export const PdfRedactor = (p: {
   onDeletionAdd: (x: TDeletion) => void;
   onDeletionRemove: (x: TDeletion) => void;
   initRedactions: TRedaction[];
+  onShowRedactionLogModal?: (redactions: TRedaction[]) => void;
 }) => {
   const { previousModeRef } = usePreviousModeRef(p.mode);
 
@@ -450,6 +457,7 @@ export const PdfRedactor = (p: {
                 await p.onSaveRedactions();
                 p.onRedactionsChange([]);
               }}
+              onShowRedactionLogModal={p.onShowRedactionLogModal}
             />
           </div>
         )}
