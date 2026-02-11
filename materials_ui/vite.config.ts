eddb9d5b38a11b236a760eae29d4fbba27704835
Daniel Barber <daniel.barber@cps.gov.uk>
2026-02-11 15:51:59 +0000

    testing unit testing task

--- materials_ui/vite.config.ts
+++ materials_ui/vite.config.ts
@@ -124,6 +124,9 @@ export default defineConfig(({ mode }) => {
     test: {
       globals: true,
       environment: 'jsdom',
+      outputFile: {
+        junit: "./unit-test-results.xml",
+      },
       setupFiles: './src/setupTests.ts',
       silent: true,
       exclude: ['e2e', 'node_modules']
