aae3f6205b591c35056f02a66fe0cf069aa7c9b8
abdul-cps <abdul.tarafdar@cps.gov.uk>
2026-03-05 15:45:00 +0000

    [FCT2-15044] - Migrate/add tests for PCD request (#210)
    * add tests for pcd-request
    
    ---------
    
    Co-authored-by: abdul-cps <abdu.tarafdar@cps.gov.uk>

---
+++ materials_ui/tests/tests-e2e/pcd-request.spec.ts
@@ -0,0 +1,59 @@
+import { expect, test } from '@playwright/test';
+import { mockRoute } from '../helpers';
+import { mockPcdCoreResponse } from '../mocks/pcd/mockPcdCore';
+import { mockPcdRequestResponse } from '../mocks/pcd/mockPcdRequest';
+
+// cases/2172759/pcds/145739/pcd-request
+test.describe('PCD Request Page', () => {
+  test('T-001: page loads list of PCD requests as expected', async ({
+    page
+  }) => {
+    mockRoute(page, 'pcds/2167259/pcd-request-core', mockPcdCoreResponse());
+    mockRoute(page, '/pcd-request', mockPcdRequestResponse());
+    await page.goto('./pcd-request/145739');
+    await page
+      .getByRole('heading', { name: 'Loading status' })
+      .waitFor({ state: 'detached' });
+
+    await expect(
+      page.getByRole('heading', { name: 'Police details' })
+    ).toBeVisible();
+
+    await expect(page.getByText('Case outline')).toBeVisible();
+    await expect(
+      page.getByText(`Supervising officer's comments`)
+    ).toBeVisible();
+    await expect(page.getByText('Proposed charges')).toBeVisible();
+    await expect(page.getByText('Bail details')).toBeVisible();
+    await expect(page.getByText('Materials provided')).toBeVisible();
+  });
+
+  test('T-002: should navigate to a specific PCD request when a link is clicked', async ({
+    page
+  }) => {
+    mockRoute(page, 'pcds/2167259/pcd-request-core', mockPcdCoreResponse());
+    mockRoute(page, '/pcd-request', mockPcdRequestResponse());
+    await page.goto('./pcd-request/145739');
+    await page
+      .getByRole('heading', { name: 'Loading status' })
+      .waitFor({ state: 'detached' });
+    await expect(
+      page.getByRole('heading', { name: 'Police details' })
+    ).toBeVisible();
+    await page.locator(`a:text("01/01/2000")`).click();
+    await expect(page.locator('dd').nth(0)).toHaveText(`02/02/2021`);
+  });
+
+  test('T-003: should display a message when no PCD requests are available', async ({
+    page
+  }) => {
+    mockRoute(page, 'pcds/2167259/pcd-request-core', []);
+    await page.goto('./pcd-request/');
+    await page
+      .getByRole('heading', { name: 'Loading status' })
+      .waitFor({ state: 'detached' });
+    await expect(
+      page.getByText('There are no PCD Requests to show.')
+    ).toBeVisible();
+  });
+});
