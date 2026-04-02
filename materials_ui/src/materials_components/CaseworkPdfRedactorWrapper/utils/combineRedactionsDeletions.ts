e0b79999152714477bd28ed41f7adefae3d74da6
robmolloy-cps <robert.molloy@cps.gov.uk>
2026-04-02 09:27:12 +0100

    FCT2-16044-be-redactions-500-error-when-save-all-redactions-button-is-pressed

--- materials_ui/src/materials_components/CaseworkPdfRedactorWrapper/utils/combineRedactionsDeletions.ts
+++ materials_ui/src/materials_components/CaseworkPdfRedactorWrapper/utils/combineRedactionsDeletions.ts
@@ -4,20 +4,6 @@ import { TDeletion } from '../../PdfRedactor/utils/deletionUtils';
 export type TRedactionDetail = { redactionId: string; randomId: string };
 export type TDeletionDetail = { deletionId: string; randomId: string };
 
-export const combineDeletionsWithDeletionDetails = (p: {
-  deletions: TDeletion[];
-  deletionDetails: TDeletionDetail[];
-}) => {
-  const deletionsWithDetails = p.deletions
-    .map((x) => {
-      const thisDetails = p.deletionDetails.find((y) => y.deletionId === x.id);
-      if (!thisDetails) return undefined;
-      return { ...x, ...thisDetails };
-    })
-    .filter((x) => !!x);
-  return deletionsWithDetails;
-};
-
 export const combineRedactionsWithRedactionDetails = (p: {
   redactions: TRedaction[];
   redactionDetails: TRedactionDetail[];
@@ -32,4 +18,18 @@ export const combineRedactionsWithRedactionDetails = (p: {
     })
     .filter((x) => !!x);
   return redactionsWithDetails;
+};
+
+export const combineDeletionsWithDeletionDetails = (p: {
+  deletions: TDeletion[];
+  deletionDetails: TDeletionDetail[];
+}) => {
+  const deletionsWithDetails = p.deletions
+    .map((x) => {
+      const thisDetails = p.deletionDetails.find((y) => y.deletionId === x.id);
+      if (!thisDetails) return undefined;
+      return { ...x, ...thisDetails };
+    })
+    .filter((x) => !!x);
+  return deletionsWithDetails;
 };
