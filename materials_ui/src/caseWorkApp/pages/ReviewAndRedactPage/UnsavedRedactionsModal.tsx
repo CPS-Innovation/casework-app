cf15ccca403c7e11fdbdb39a10f2529e3a002028
robmolloy-cps <robert.molloy@cps.gov.uk>
2026-04-08 16:05:46 +0100

    FCT2-15821-das-jaws-does-the-screen-reader-repeat-what-you-can-read-issue-d

--- materials_ui/src/caseWorkApp/pages/ReviewAndRedactPage/UnsavedRedactionsModal.tsx
+++ materials_ui/src/caseWorkApp/pages/ReviewAndRedactPage/UnsavedRedactionsModal.tsx
@@ -69,37 +69,6 @@ export const UnsavedRedactionsModal = ({
           </>
         }
       />
-      <div style={{ padding: '20px', background: 'white' }}>
-        <div>
-          You have {documentsWithRedactions.length} documents with unsaved
-          redactions
-        </div>
-        <br />
-        <div style={{ display: 'flex', gap: '4px', flexDirection: 'column' }}>
-          {documentsWithRedactions.map((doc) => (
-            <a
-              className="govuk-link"
-              key={doc.documentId}
-              onClick={() => onDocumentClick(doc.documentId)}
-            >
-              {doc.presentationTitle}
-            </a>
-          ))}
-        </div>
-        <br />
-        <div>
-          If you do not save the redactions the file will not be changed.
-        </div>
-        <br />
-        <div style={{ display: 'flex', gap: '16px' }}>
-          <Button variant="primary" onClick={onReturnClick}>
-            Return to case file
-          </Button>
-          <Button variant="inverse" onClick={onIgnoreClick}>
-            Ignore
-          </Button>
-        </div>
-      </div>
     </Modal>
   );
 };
