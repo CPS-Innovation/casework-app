871596ce82646f56c1cd1d81122d37d1c43844cf
TahmeedAhmed-NTT-CPS <Tahmeed.Ahmed@cps.gov.uk>
2026-04-15 10:33:15 +0100

    FE impl to render error messages (#264)

--- materials_ui/src/components/DocumentPreview/DocumentPreview.tsx
+++ materials_ui/src/components/DocumentPreview/DocumentPreview.tsx
@@ -3,6 +3,7 @@ import { CaseMaterialsType } from '../../schemas/caseMaterials';
 import { ErrorSummary } from '../ErrorSummary/ErrorSummary';
 import { LoadingSpinner } from '../LoadingSpinner/LoadingSpinner';
 import { PdfViewer } from '../PdfViewer/PdfViewer';
+import { Banner } from '../../components';
 
 type Props = { row: CaseMaterialsType };
 
@@ -13,22 +14,46 @@ export default function DocumentPreview({ row }: Props) {
     error: caseDocumentError
   } = useDocumentPreview({ materialId: row.materialId });
 
+  const errorTitle = caseDocumentError?.toString().includes('403') ?
+    'This document is password protected' : 'No preview available';
+
+  const errorMessage = caseDocumentError?.toString().includes('403') ?
+    'Ask the agency who supplied it to remove the password and resend the document.' :
+    'The material you are trying to preview is not currently available. You can view it in CMS.';
+
+  let content = null;
+
+  if (!caseDocumentLoading) {
+    if (caseDocumentError) {
+      content = caseDocumentError.toString().includes('403') ? (
+        <Banner
+          type="error"
+          header={errorTitle}
+          content={errorMessage}
+        />
+      ) : (
+        <ErrorSummary
+          errorTitle={errorTitle}
+          errorMessage={errorMessage}
+        />
+      );
+    } else if (caseDocumentData) {
+      content = (
+        <PdfViewer
+          file={caseDocumentData}
+          fileName={row.subject}
+        />
+      );
+    }
+  }
+
   return (
     <>
       <LoadingSpinner
         isLoading={caseDocumentLoading}
         textContent="Loading preview..."
       />
-      {!caseDocumentLoading && (
-        caseDocumentError ? (
-          <ErrorSummary
-            errorTitle="No preview available"
-            errorMessage="The material you are trying to preview is not currently available. You can view it in CMS."
-          />
-        ) : caseDocumentData ? (
-          <PdfViewer file={caseDocumentData} fileName={row.subject} />
-        ) : null
-      )}
+      {content}
     </>
   );
 }
